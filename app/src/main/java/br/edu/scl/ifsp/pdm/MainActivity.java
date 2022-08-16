1557a74275713c53a4000dccc5fc85a781806e99
Gabriel Borges <gabrielborges565@gmail.com>
2022-08-16 00:20:40 -0300

    Pegando referencias pelos ids

--- app/src/main/java/br/edu/scl/ifsp/pdm/MainActivity.java
+++ app/src/main/java/br/edu/scl/ifsp/pdm/MainActivity.java
@@ -1,15 +1,45 @@
 package br.edu.scl.ifsp.pdm;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editNome;
+    private EditText editTelefone;
+    private EditText editEmail;
+    private CheckBox checkBox;
+    private RadioButton rdMasc;
+    private RadioButton rdFemin;
+    private EditText editCidade;
+    private Spinner spinnerUf;
+
+    private Button btnSalvar;
+    private Button btnLimpar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editNome = findViewById(R.id.editNome);
+        editTelefone = findViewById(R.id.editTelefone);
+        editEmail = findViewById(R.id.editEmail);
+        checkBox = findViewById(R.id.checkBox);
+        rdMasc = findViewById(R.id.rdMasc);
+        rdFemin = findViewById(R.id.rdFemin);
+        editCidade = findViewById(R.id.editCidade);
+        spinnerUf = findViewById(R.id.spinnerUf);
+
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnLimpar = findViewById(R.id.btnLimpar);
+
     }
 }
